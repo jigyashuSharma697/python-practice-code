47a634bcc08ea9da0e3d3893fbb761be8f29f86d
Jigyashu sharma <jigyashusharma697@gmail.com>
2026-09-22 08:42:30 +0530

    python v2

--- hello.py
+++ hello.py
@@ -1,23 +1,57 @@
-#  Input we need from the user
-# Total rent.
-# Total food ordered for snaking
-# Electricity units spend
-# Charge per unit
-# persons living in Room/Flat
+"""
+WORKFLOW OF PROJECT:
+1- Input from user(Rock,Paper,Scissor)
+2- Computer choice (Computer will choice randomly not conditionally)
+3- Result print
 
+Cases:
+A- Rock
+Rock - Rock = tie
+Rock - Paper = Paper win
+Rock - Scissor = Rock win
 
-#   Output
-# Total amount you've to pay is
+B - Paper
+Paper - Paper = tie
+Paper - Rock = Paper win
+Paper - Scissor = Scissor win
 
-rent = int(input("Enter your Hostel/Flat rent = "))
-food = int(input("Enter your amount of food  ordered = "))
-electricitySpend = int(input("Enter the total of electricity Spend = "))
-ChargePerUnit = int(input("Enter the charge per unit = "))
-persons = int(input("Enter the number of person living in room/flat = "))
+C - Scissor 
+Scissor - Scissor = Tie
+Scissor - Rock = Rock tie
+Scissor - Paper = Scissor win
 
+"""
 
-totalBill = electricitySpend * ChargePerUnit
+import random
+itemList = ["Rock", "Paper", "Scissor"]
 
-output = (food + rent + totalBill) // persons
+userChoice = input("Enter your move = Rock , Paper, Scissor = ")
+compChoice = random.choice(itemList)
 
-print("Each person will pay = ", output)
+print(f"User choice = {userChoice}, Computer Choice = {compChoice}")
+
+if userChoice == compChoice:
+    print("Both chooese same: Match tie")
+    
+elif userChoice ==  "Rock":
+    if compChoice == "Paper":
+     print("Paper covers Rocks = Computer")
+    
+    else:
+        print("Rock smashes Scissor = You win")
+        
+elif userChoice == "Paper":   
+    if compChoice == "Scissor":
+        print("Scissor cuts Paper, Computer win")
+        
+    else:
+        print("Paper cover rock, You win")
+        
+elif userChoice == "Scissor":
+    if compChoice == "Paper":
+     print("Scissor cuts paper, You win")
+     
+else:
+    print("Rock smashes scissor , Computer win")
+
+     
