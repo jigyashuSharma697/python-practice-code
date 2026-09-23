bc5ae5539344f3450f4ecbe8bac2729fea2eae6c
Jigyashu sharma <jigyashusharma697@gmail.com>
2026-09-23 21:50:40 +0530

    clock

--- hello.py
+++ hello.py
@@ -1,39 +1,17 @@
 import tkinter as tk
-from tkinter import messagebox
+from time import strftime
 
-def checkWinner():
-    for combo in [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]:
-        if buttons[combo[0]]["text"] == buttons[combo[1]]["text"] == buttons[combo[2]]["text"] !="":
-            buttons[combo[0]].config(bg="green")
-            buttons[combo[1]].config(bg="green")
-            buttons[combo[2]].config(bg="green")
-            messagebox.showinfo("Tic-Tac-Toe", f"Player {buttons[combo[0]]["text"]}wins!")
-            root.quit()
-            
-            
-def buttonClick(index):
-    if buttons[index]["text"] == ""  and not winner:
-        buttons[index]["text"] = currentPlayer
-        checkWinner()
-        togglePlayer()
-        
-def togglePlayer():
-    global currentPlayer
-    currentPlayer = "x" if currentPlayer == "0" else "0"
-    label.config(text=f"Player {currentPlayer}'s turn")
-    
 root = tk.Tk()
-root.title("Tic-Tac-Toe")
+root.title("Digital Clock")
 
-buttons = [tk.Button(root, text="", font=("normal",25), width=6, height=2, command=lambda i=i: buttonClick(i)) for i in range(9)]
-
-for i, button in enumerate(buttons):
-    button.grid(row=i//3, column=i % 3)
+def time():
+    string = strftime('%I:%M:%S %p \n %d/%m/%Y')
+    label.config(text=string)
+    label.after(1000,time)
     
-currentPlayer = "x"
-winner = False
+label = tk.Label(root, font=('calibri', 50,'bold'), background='yellow',foreground='black')
+label.pack(anchor='center')
 
-label = tk.Label(root, text=f"Player{currentPlayer}'s turn", font=("normal", 16))
-label.grid(row=3, column=0, columnspan=3)
+time()
 
-root.mainloop ()
+root.mainloop()
